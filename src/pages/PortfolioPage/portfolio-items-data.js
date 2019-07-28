39fea52ecf2efe089077ed2f051c07d9f8f77e3c
Trung Le <trung.le@seldatinc.com>
2019-07-28 23:47:42 +0700

    test

--- src/pages/PortfolioPage/portfolio-items-data.js
+++ src/pages/PortfolioPage/portfolio-items-data.js
@@ -2,53 +2,53 @@ import React from 'react';
 
 export default [{
    first: [{
-    title: 'Typy',
+    title: 'Typo',
     desc: 'JavaScript type checking library with a sweeter syntax',
     link: 'https://github.com/flexdinesh/dev-landing-page'
   }, {
-    title: 'Typy',
+    title: 'Typo',
     desc: 'JavaScript type checking library with a sweeter syntax',
     link: 'https://github.com/flexdinesh/dev-landing-page'
   }, {
-    title: 'Typy',
+    title: 'Typo',
     desc: 'JavaScript type checking library with a sweeter syntax',
     link: 'https://github.com/flexdinesh/dev-landing-page'
   },{
-    title: 'Typy',
+    title: 'Typo',
     desc: 'JavaScript type checking library with a sweeter syntax',
     link: 'https://github.com/flexdinesh/dev-landing-page'
   },{
-    title: 'Typy',
+    title: 'Typo',
     desc: 'JavaScript type checking library with a sweeter syntax',
     link: 'https://github.com/flexdinesh/dev-landing-page'
   }]
 },
 {
   second: [{
-    title: 'Typy',
+    title: 'Typo',
     desc: 'JavaScript type checking library with a sweeter syntax',
     link: 'https://github.com/flexdinesh/dev-landing-page'
   }, {
-    title: 'Typy',
+    title: 'Typo',
     desc: 'JavaScript type checking library with a sweeter syntax',
     link: 'https://github.com/flexdinesh/dev-landing-page'
   }]
 },
 {
   third: [{
-    title: 'Typy',
+    title: 'Typo',
     desc: 'JavaScript type checking library with a sweeter syntax',
     link: 'https://github.com/flexdinesh/dev-landing-page'
   }, {
-    title: 'Typy',
+    title: 'Typo',
     desc: 'JavaScript type checking library with a sweeter syntax',
     link: 'https://github.com/flexdinesh/dev-landing-page'
   }, {
-    title: 'Typy',
+    title: 'Typo',
     desc: 'JavaScript type checking library with a sweeter syntax',
     link: 'https://github.com/flexdinesh/dev-landing-page'
   },{
-    title: 'Typy',
+    title: 'Typo',
     desc: 'JavaScript type checking library with a sweeter syntax',
     link: 'https://github.com/flexdinesh/dev-landing-page'
   }]
